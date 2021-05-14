1a79b9c7c175e35137ec8554418cb1a075df12e1
Guilherme Lellis <lguilherme44@gmail.com>
2021-05-14 20:03:09 -0300

    aula     Componente: NewTransactionModal

--- gvmoney/src/App.tsx
+++ gvmoney/src/App.tsx
@@ -1,12 +1,32 @@
+import { useState } from 'react';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 export function App() {
+   const [isNewTransactionModalOpen, SetIsNewTransactionModalOpen] =
+      useState(false);
+
+   function handleOpenNewTransactionModal() {
+      SetIsNewTransactionModalOpen(true);
+   }
+
+   function handleCloseNewTransactionModal() {
+      SetIsNewTransactionModalOpen(false);
+   }
+
    return (
       <>
-         <Header />
+         <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
          <Dashboard />
+
+         <NewTransactionModal
+            isOpen={isNewTransactionModalOpen}
+            onRequestClose={handleCloseNewTransactionModal}
+         >
+            <h2>Cadastrar transação</h2>
+         </NewTransactionModal>
          <GlobalStyle />
       </>
    );
